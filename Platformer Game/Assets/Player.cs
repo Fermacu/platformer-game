15ada1efcf826de2f80e337536b522f7db39d0bd
Fernando Matamoros <fer.andres@hotmail.com>
2025-11-28 01:44:10 -0600

    Add double jump, wall slide, and animation logic
    Enhanced the Player controller with double jump and wall slide mechanics, improved collision detection, and integrated animation parameter updates. Refactored movement, input handling, and flipping logic for better responsiveness and code organization.

--- Platformer Game/Assets/Player.cs
+++ Platformer Game/Assets/Player.cs
@@ -7,23 +7,148 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Animator anim;
+
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
+
+    private bool canDoubleJump;
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    private bool isAirborne;
+    private bool isWallDetected;
 
     private float xInput;
+    private float yInput;
+
+    private bool facingRight = true;
+    private int facingDir = 1;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+
+        UpdateAirborneStatus(); 
+        HandleInput();
+        HandleWallSlide();
+        HandleMovement();
+        HandleFlip();
+        HandleCollision();
+        HandleAnimations();
+    }
+
+    private void UpdateAirborneStatus()
+    {
+        if (isAirborne && isGrounded)
+           HandleLanding();
+
+        if (!isGrounded && !isAirborne)
+            BecomeAirborne();
+    }
+
+    private void BecomeAirborne()
+    {
+        isAirborne = true;
+    }
+
+    private void HandleWallSlide()
+    {
+        bool canWallSlide = isWallDetected && rb.linearVelocity.y < 0;
+        float yModifier = yInput < 0 ? 1: .05f;
+
+        if (canWallSlide == false)
+            return;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * yModifier);
+        
+    }
+
+    private void HandleLanding()
+    {
+        isAirborne = false;
+        canDoubleJump = true;
+    }
+
+    private void HandleInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-       HandleMovement();
+        yInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            JumpButton();
+        
+    }
+
+    private void JumpButton()
+    {
+       if(isGrounded)
+        {
+            Jump();
+        }
+         else if (canDoubleJump)
+        {
+           DoubleJump();
+        }
+    }
+
+    private void Jump() => rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+    private void DoubleJump() 
+    { 
+        canDoubleJump = false;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, doubleJumpForce);
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isWallDetected", isWallDetected);
     }
 
     private void HandleMovement()
     {
+        if(isWallDetected)
+            return;
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+    private void HandleFlip()
+    {
+        if (xInput < 0 && facingRight || xInput > 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + wallCheckDistance * facingDir, transform.position.y));
     }
 }
