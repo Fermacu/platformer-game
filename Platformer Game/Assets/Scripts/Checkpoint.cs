79dbfac67eeed28e93a5fc078c94e3012c1c5b7c
Fernando Matamoros <fer.andres@hotmail.com>
2025-12-01 02:36:24 -0600

    Add checkpoint system with animations and prefab
    Introduced a new checkpoint feature including Checkpoint.cs script, prefab, and related animations. Updated GameManager to support respawn position updates and adjusted sprite import settings for checkpoint graphics.

---
+++ Platformer Game/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Animator anim => GetComponent<Animator>();
+    private bool active;
+
+    [SerializeField] private bool canBeReactivated;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(active && canBeReactivated == false) 
+            return;
+
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            ActivateCheckpoint();
+        }
+    }
+
+    private void ActivateCheckpoint()
+    {
+        active = true;
+        anim.SetTrigger("activate", active);
+        GameManager.instance.UpdateRespawnPosition(transform);
+    }
+}
