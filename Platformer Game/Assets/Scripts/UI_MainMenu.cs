cd32d5b4c022b4cc0d5998c9954a803d32f0539c
Fernando Matamoros <fer.andres@hotmail.com>
2025-12-03 12:17:31 -0600

    Refactor main menu UI and add skin selection
    Reworked the MainMenu.unity scene to introduce a new UI structure, including a NextSkin button and a Select button for skin selection. Updated the NewGame button to use a SwitchUI method for UI transitions, and adjusted text and layout for improved navigation. These changes prepare the main menu for additional features such as skin and level selection.

--- Platformer Game/Assets/Scripts/UI_MainMenu.cs
+++ Platformer Game/Assets/Scripts/UI_MainMenu.cs
@@ -5,8 +5,20 @@ public class UI_MainMenu : MonoBehaviour
 {
     public string sceneName;
 
+    [SerializeField] private GameObject[] uiElements;
+    
     public void NewGame()
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void SwitchUI(GameObject uiToEnable)
+    {
+        foreach (GameObject ui in uiElements)
+        {
+            ui.SetActive(false);
+        }
+
+        uiToEnable.SetActive(true);
     }
 }
